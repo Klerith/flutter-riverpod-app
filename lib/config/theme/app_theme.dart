fca2d3efcb212a50ff750529334db20586eea5c7
Fernando Herrera <fernando.herrera85@gmail.com>
2023-03-30 10:37:52 -0400

    first commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: Colors.purpleAccent
+  );
+
+}
