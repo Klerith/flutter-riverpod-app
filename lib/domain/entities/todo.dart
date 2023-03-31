66aa3a1610563fef7d29bc468a192c6421f63eb4
Fernando Herrera <fernando.herrera85@gmail.com>
2023-03-31 10:08:45 -0400

    Todo entity agregado

---
+++ lib/domain/entities/todo.dart
@@ -0,0 +1,29 @@
+
+
+class Todo {
+
+  final String id;
+  final String description;
+  final DateTime? completedAt;
+
+  Todo({
+    required this.id,
+    required this.description,
+    required this.completedAt,
+  });
+
+  bool get done {
+    return completedAt == null; // true or false
+  }
+
+  Todo copyWith({
+    String? id,
+    String? description,
+    DateTime? completedAt,
+  }) => Todo(
+    id: id ?? this.id,
+    description: description ?? this.description,
+    completedAt: completedAt ?? this.completedAt,
+  );
+
+}
