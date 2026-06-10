37bcd56891d504f2a646452c117730b3d5709511
Fernando Herrera <fernando.herrera85@gmail.com>
2026-06-10 08:54:45 -0400

    Refactor main function to use ProviderScope and add custom_lint plugin to analyzer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_app/config/config.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
